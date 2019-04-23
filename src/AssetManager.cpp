c72c6e4ee54436db2307f36cd5efe9cd5bf3a898
Adrian Lizarraga <adrianlm2@gmail.com>
2019-04-22 20:57:34 -0700

    Clean up a few things in asset manager

--- src/AssetManager.cpp
+++ src/AssetManager.cpp
@@ -1,49 +1,45 @@
 #include "AssetManager.h"
 #include <iostream>
 
-TextureKey::TextureKey(const std::string& filename, bool smooth, bool repeated): filename(filename), smooth(smooth), repeated(repeated) {
-
-}
+TextureKey::TextureKey(const std::string &filename, bool smooth, bool repeated) : filename(filename), smooth(smooth), repeated(repeated) {}
 
 bool operator<(const TextureKey &k1, const TextureKey &k2) {
     int nameComparison = k1.filename.compare(k2.filename);
-    
+
     if (nameComparison != 0) {
         return nameComparison < 0;
-    }
-    else if (k1.smooth != k2.smooth) {
+    } else if (k1.smooth != k2.smooth) {
         return k1.smooth < k2.smooth;
-    }
-    else {
+    } else {
         return k1.repeated < k2.repeated;
     }
 }
 
 AssetManager::AssetManager() {}
 
-bool AssetManager::hasTexture(const TextureKey& key) const {
+bool AssetManager::hasTexture(const TextureKey &key) const {
     auto it = this->textures.find(key);
-    
+
     return it != this->textures.end();
 }
 
-bool AssetManager::hasTexture(const std::string& filename, bool smooth, bool repeated) const {
+bool AssetManager::hasTexture(const std::string &filename, bool smooth, bool repeated) const {
     TextureKey key(filename, smooth, repeated);
 
     return this->hasTexture(key);
 }
 
-bool AssetManager::hasFont(const std::string& filename) const {
+bool AssetManager::hasFont(const std::string &filename) const {
     auto it = this->fonts.find(filename);
 
     return it != this->fonts.end();
 }
 
-bool AssetManager::loadTexture(const TextureKey& key) {
+bool AssetManager::loadTexture(const TextureKey &key) {
     if (this->hasTexture(key)) {
         return true;
     }
-    
+
     std::unique_ptr<sf::Texture> texture = std::make_unique<sf::Texture>();
     bool loaded = texture->loadFromFile(key.filename);
 
@@ -52,7 +48,6 @@ bool AssetManager::loadTexture(const TextureKey& key) {
         texture->setSmooth(key.smooth);
 
         this->textures.emplace(std::make_pair(key, std::move(texture)));
-        std::cout << "Loaded " << key.filename << std::endl;
     }
 
     return loaded;
@@ -68,30 +63,29 @@ bool AssetManager::loadFont(const std::string &filename) {
     if (this->hasFont(filename)) {
         return true;
     }
-    
+
     std::unique_ptr<sf::Font> font = std::make_unique<sf::Font>();
     bool loaded = font->loadFromFile(filename);
 
     if (loaded) {
         this->fonts.emplace(std::make_pair(filename, std::move(font)));
-        std::cout << "Loaded " << filename << std::endl;
     }
 
     return loaded;
 }
 
-sf::Texture& AssetManager::getTexture(const TextureKey& key) {
+sf::Texture &AssetManager::getTexture(const TextureKey &key) {
     this->loadTexture(key);
     return *this->textures.at(key);
 }
 
-sf::Texture& AssetManager::getTexture(const std::string &filename, bool smooth, bool repeated) {
+sf::Texture &AssetManager::getTexture(const std::string &filename, bool smooth, bool repeated) {
     TextureKey key(filename, smooth, repeated);
-    
+
     return this->getTexture(key);
 }
 
-sf::Font& AssetManager::getFont(const std::string &filename) {
+sf::Font &AssetManager::getFont(const std::string &filename) {
     this->loadFont(filename);
     return *this->fonts.at(filename);
 }
