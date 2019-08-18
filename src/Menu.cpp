659b2dddf8e6d007e2a7b25891e8ccb967feeb03
Adrian Lizarraga <adrianlm2@gmail.com>
2019-08-17 21:17:58 -0700

    Set mode in pause menu

--- src/Menu.cpp
+++ src/Menu.cpp
@@ -1,21 +1,24 @@
 #include "Menu.h"
 #include "Game.h"
 
-Menu::Menu(Game* game): m_game(game) {
-}
+Menu::Menu(Game *game) : m_game(game) {}
 
 void Menu::handleEvent(sf::Event event) {
     if (event.type == sf::Event::KeyPressed) {
         if (event.key.code == sf::Keyboard::P) {
             m_game->setPaused(!m_game->getPaused());
+        } else if (event.key.code == sf::Keyboard::Num0) {
+            m_game->setMode(0);
+        } else if (event.key.code == sf::Keyboard::Num1) {
+            m_game->setMode(1);
+        } else if (event.key.code == sf::Keyboard::Num2) {
+            m_game->setMode(2);
+        } else if (event.key.code == sf::Keyboard::Escape) {
+            m_game->setPaused(false);
         }
     }
 }
 
-void Menu::update(float elapsed) {
+void Menu::update(float elapsed) {}
 
-}
-
-void Menu::draw(sf::RenderWindow& window) const {
-
-}
+void Menu::draw(sf::RenderWindow &window) const {}
